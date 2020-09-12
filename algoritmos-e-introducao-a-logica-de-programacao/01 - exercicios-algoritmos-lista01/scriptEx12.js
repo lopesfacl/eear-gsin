847183bb66a51bb496d2b89d74e0cde0428bf798
Alisson Costa <f.alissoncosta96@gmail.com>
2020-09-12 01:12:09 -0300

    gsin v1 correcao questao 12 lista 01

--- algoritmos-e-introducao-a-logica-de-programacao/01 - exercicios-algoritmos-lista01/scriptEx12.js
+++ algoritmos-e-introducao-a-logica-de-programacao/01 - exercicios-algoritmos-lista01/scriptEx12.js
@@ -1,28 +1,34 @@
 function valorCombustivel(litros, tipo) {
 	var taxa_desconto = null;
-	var preco_sem_desconto = litros * 2.99;
+	var preco_por_litro = null;
+	var preco_sem_desconto = null;
 	var valor_desconto = null;
 	switch (tipo) {
 	case 'A':
-		if (litros <= 20) {
+		preco_por_litro = 2.19;
+		preco_sem_desconto = litros * preco_por_litro;
+		if (litros <= 20) {			
 			taxa_desconto = 3 / 100;
-			valor_desconto = litros * taxa_desconto;
+			valor_desconto = preco_sem_desconto * taxa_desconto;
 			preco_com_desconto = preco_sem_desconto - valor_desconto;
 			return preco_com_desconto;
 		}
+		
 		taxa_desconto = 5 / 100;
-		valor_desconto = litros * taxa_desconto;
+		valor_desconto = preco_sem_desconto * taxa_desconto;
 		preco_com_desconto = preco_sem_desconto - valor_desconto;
 		return preco_com_desconto;
 	case 'G':
+		preco_por_litro = 2.99;
+		preco_sem_desconto = litros * preco_por_litro;
 		if (litros <= 20) {
 			taxa_desconto = 4 / 100;
-			valor_desconto = litros * taxa_desconto;
+			valor_desconto = preco_sem_desconto * taxa_desconto;
 			preco_com_desconto = preco_sem_desconto - valor_desconto;
 			return preco_com_desconto;
 		}
 		taxa_desconto = 6 / 100;
-		valor_desconto = litros * taxa_desconto;
+		valor_desconto = preco_sem_desconto * taxa_desconto;
 		preco_com_desconto = preco_sem_desconto - valor_desconto;
 		return preco_com_desconto;
 
